9b2903540779c66dfa3bfa2f9b6beb7c9a3fde99
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-07-17 03:01:23 +0800

    Init project

---
+++ README.md
@@ -0,0 +1,6 @@
+Rails nested model form practice
+
+Video: https://www.youtube.com/watch?v=amT27SfNhKM
+Github source: https://github.com/railscasts/196-nested-model-form-revised/tree/master/questionnaire-after
+
+This app use Rails 2.2.2
---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,54 @@
+class SurveysController < ApplicationController
+
+  before_action :set_survey, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @surveys = Survey.all
+  end
+
+  def show
+    
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+    @survey = Survey.new(survey_params)
+    if @survey.save
+      redirect_to @survey, notice: "Successfully created survey."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  
+  end
+
+  def update
+    #@survey = Survey.find(survey_params)
+    #raise
+    if @survey.update(survey_params)
+      redirect_to @survey, notice: "Successfully updated survey."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @survey.destroy
+    redirect_to surveys_url, notice: "Successfully destroyed survey."
+  end
+
+private
+  
+  def set_survey
+    @survey = Survey.find(params[:id])
+  end
+
+  def survey_params
+    params.require(:survey).permit(:name, :questions_attributes)
+  end
+end
