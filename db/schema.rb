6274fcab59acb50811c7c84aaef9bae5dea433ff
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-07-18 08:11:41 +0800

    Add quantity column inside answers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120417221635) do
+ActiveRecord::Schema.define(version: 20150718001039) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity"
   end
 
   create_table "questions", force: :cascade do |t|
